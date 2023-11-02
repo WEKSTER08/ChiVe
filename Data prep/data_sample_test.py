77975fb9b9e97169822fa57307b8c855d6672be2
Sriyugesh Bhyravajjula <bhyravajjula19512@mechyd.ac.in>
2023-11-02 07:47:44 +0000

    data prep v1

--- Data prep/data_sample_test.py
+++ Data prep/data_sample_test.py
@@ -1,15 +1,15 @@
 # import scipy.io
 # mat = scipy.io.loadmat('data/syl1.mat')
-import pandas as pd
+# import pandas as pd
 # print(mat)
 import os
 from mat4py import loadmat
 
 data = loadmat('data/phn1.mat')
-print(data)
+# print(data)
 
 # duration = []
-# sylStart = []
+# sylStart = [0]
 # for i in range(len(data['spurtSylTimes'])):
 #     duration.append(data['spurtSylTimes'][i][1] - data['spurtSylTimes'][i][0])
 #     sylStart.append(data['spurtSylTimes'][i][0])
@@ -17,6 +17,16 @@ print(data)
 # data['sylStart'] = sylStart
 # print(data)
 
+
+
+duration = []
+phnStart = [0]
+for i in range(len(data['phnTimes'])):
+    duration.append(round(data['phnTimes'][i][1] - data['phnTimes'][i][0],2))
+    phnStart.append(data['phnTimes'][i][0])
+data['phnTimes'] = duration
+data['phnStart'] = phnStart
+print(data)
 # new = pd.DataFrame.from_dict(data)
 
 # print(new)
@@ -29,3 +39,26 @@ print(data)
 # for files in folder:
 #     # cur_folder = os.listdir(folder + "/")
 #     print(files)
+## populating sample frequency
+# out = []
+# count=0
+# for i in range(500):
+#     if data['sylStart'][count]*1000 == i:
+#         count+=1
+#         out.append(1)
+#     else: out.append(0)
+# print(out)
+
+## population phoneme duration
+
+out_ph = []
+count_ph = 0
+
+for i in range(500):
+    if data['phnStart'][count_ph]*1000 == i:
+        count_ph+=1
+
+    else:
+        out_ph.append(data['phnTimes'][count_ph-1]*1000)
+
+print(out_ph)
