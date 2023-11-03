a6275fe856c1716f8897363c000e92dfe4d7fcd3
Sriyugesh Bhyravajjula <bhyravajjula19512@mechyd.ac.in>
2023-11-03 09:02:42 +0000

    data prep 3/11

--- Data prep/prosodic_feature_ext.py
+++ Data prep/prosodic_feature_ext.py
@@ -1,38 +1,46 @@
+
 import librosa
-import math
+import librosa.display
+import matplotlib.pyplot as plt
 import numpy as np
-y, sr = librosa.load('data/wav/ISLE_SESS0003_BLOCKD01_03_sprt1.wav',sr=16000)
-y1, sr1 = librosa.load('data/aud1.wav')
-f0, voiced_flag, voiced_probs = librosa.pyin(y, sr = sr, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'),frame_length= 20, hop_length= 10 )
-times = librosa.times_like(f0)
 
-print("sr is = ",sr,"len of y = ", len(y),"sr1 is = ",sr1,"len of y1 = ", len(y1))
+## To equal the length of the f0 and c0 values by averaging
+def average_reduce(input_list, target_length):
+    original_length = len(input_list)
+    dimension = len(input_list[0])  # Assuming all elements have the same dimensionality
+    step = original_length // target_length
 
+    reduced_list = [
+        np.mean(input_list[i:i+step], axis=0) for i in range(0, original_length, step)
+    ]
+
+    return reduced_list[:target_length]
+# Load the audio file
+# file_path = 'path/to/your/audio/file.wav'
+y, sr = librosa.load('data/wav/ISLE_SESS0003_BLOCKD01_03_sprt1.wav',sr=16000,)
+
+# Compute the short-time Fourier transform (STFT)
+D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
+
+# Extract the frequency values
+frequencies = librosa.fft_frequencies(sr=16000)
+
+# # Plot the spectrogram
+# librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log')
+# plt.colorbar(format='%+2.0f dB')
+# plt.title('Spectrogram')
+# plt.xlabel('Time')
+# plt.ylabel('Frequency')
+# plt.show()
+
+# Print the extracted frequency values
 mfcc_val = librosa.feature.mfcc(y=y, n_mfcc = 12, sr=sr,hop_length= 10)
+mfcc_val = mfcc_val.T
+f0, voiced_flag, voiced_probs = librosa.pyin(y, sr = sr, fmin=librosa.note_to_hz('C1'), fmax=librosa.note_to_hz('C7'),frame_length= 20, hop_length= 10 )
+print(mfcc_val.shape)
+reduced_mfcc = average_reduce(mfcc_val,len(frequencies))
+frequencies = frequencies.reshape(-1,1)
+frnn_inp = np.concatenate((reduced_mfcc,frequencies),axis =1)
 
-print ("f0 are =",f0.shape,"c0 are =", mfcc_val.shape)
 
-duration = librosa.get_duration(y=y, sr=sr)
-#frames per millisecond
-fpms = math.ceil(len(f0)/(duration*1000))
-
-print("frames per millisecond", fpms)
-
-# #for phrnn input 
-# f_avg = []
-# # smoothening factor
-# sf = 0.9
-# for i in range(len(f0)-1):
-#     diff = 0
-#     if i == 0:
-#         print(i)
-#         f_avg[i] = f0[i]
-       
-#     else: 
-#         diff = f0[i] - f0[i-1]
-#         diff = diff*sf
-#         f_avg[i] = f0[i-1] + diff
-
-if f0[0] == 'nan':
-    print("Hi")
-print(f0[0].dtype)
+print((frequencies.shape),(frnn_inp.shape))
