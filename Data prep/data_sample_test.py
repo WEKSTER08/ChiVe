849491e25dfe3ec6a2dc1496714664b725f2a1b4
Sriyugesh Bhyravajjula <bhyravajjula19512@mechyd.ac.in>
2023-11-13 07:37:15 +0000

    Update 13/11

--- Data prep/data_sample_test.py
+++ Data prep/data_sample_test.py
@@ -15,7 +15,7 @@ data = loadmat('data/phn1.mat')
 
 def syl_data(data,data_len):
     duration = []
-    sylStart = [0]
+    sylStart = []
     for i in range(len(data['spurtSylTimes'])):
         duration.append(round(data['spurtSylTimes'][i][1] - data['spurtSylTimes'][i][0],2))
         sylStart.append(round(data['spurtSylTimes'][i][0],2))
@@ -32,7 +32,7 @@ def syl_data(data,data_len):
             count+=1
             out.append(1)
         else: out.append(0)
-    print(out)
+    print(data)
     return out
 # print(data)
 
@@ -51,25 +51,25 @@ def phn_data(data,data_len):
     count_ph = 0
     factor = int(data_len/(phnStart[-1]*100))
     cut_off = len(phnStart)
-    (print(cut_off))
+    # (print(cut_off))
     # print(factor,phnStart[-1])
     for i in range(data_len):
         
         if int(data['phnStart'][count_ph]*100*factor) == i:
             # print(data['phnStart'][count_ph]*100*factor,i)
             if(count_ph == cut_off-1) : 
-                print(i)
+                # print(i)
                 out_ph.append(data['phnTimes'][count_ph-1]*1000)
                 continue
             else :
                 count_ph+=1
-                print(i,data['phnTimes'][count_ph-1]*1000)
+                # print(i,data['phnTimes'][count_ph-1]*1000)
                 out_ph.append(data['phnTimes'][count_ph-1]*1000)
 
         else:
             out_ph.append(data['phnTimes'][count_ph-1]*1000)
             # print(i)
-    print()
+    # print()
     return out_ph
 # print(data)
 # new = pd.DataFrame.from_dict(data)
@@ -100,13 +100,13 @@ def phn_data(data,data_len):
 
 # print(out_ph)
 ## To iterate over all the files
-files  = os.listdir("data/phn")
+files  = os.listdir("data/syl")
 outs = []
 data_len = 1200
 for i,file in enumerate(files):
     if i == 1: break
-    data = loadmat("data/phn/"+file)
-    outs.append(phn_data(data,data_len))
+    data = loadmat("data/syl/"+file)
+    outs.append(syl_data(data,data_len))
 
 print(len(outs[0]))
 
@@ -133,7 +133,7 @@ for chars in tokens:
 ## vectorizing sentences 
 from gensim.test.utils import common_texts
 from gensim.models import Word2Vec
-model = Word2Vec(sentences=sentences, vector_size=13, window=1, min_count=1, workers=4)
+model = Word2Vec(sentences=sentences, vector_size=32, window=1, min_count=1, workers=4)
 model.save("word2vec.model")
 word2vec_model = model
 
@@ -159,4 +159,33 @@ for i in range(len(sentences)):
     sentence_vectors.append(word_vecs)
     word_vecs = []
 
-# print(sentence_vectors[2])
+# print(sentence_vectors[0])
+
+def syl_val(vector,sample_freq,data_len):
+    zero_val = np.zeros(32)
+    out_vec= []
+    count = 0
+    print(sample_freq[0])
+    print(vector)
+    print("hi")
+    for i in range(data_len):
+        if sample_freq[i] == 1:
+            print(count)
+            if count == len(vector):
+                out_vec.append(zero_val)
+                continue
+            else:
+                out_vec.append(vector[count])
+                count +=1
+        else: out_vec.append(zero_val)
+    return out_vec
+
+
+syl_v = syl_val(sentence_vectors[0],outs[0],data_len)
+print(len(syl_v))
+
+
+
+
+
+
